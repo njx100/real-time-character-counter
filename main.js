c37305ad6b9024e98744fdfed6e3a07c4ed83875
njx100 <dnnguyen100@gmail.com>
2023-03-27 11:22:34 +0700

    complete project

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+const textEl = document.getElementById("text");
+const totalEl = document.getElementById("total");
+const remainingEl = document.getElementById("remaining");
+
+function countChar() {
+    let count = textEl.value;
+    return count.length;
+}
+
+function remainingChar() {
+    let r = 50 - countChar();
+    return r;
+}
+
+textEl.addEventListener("keyup", () => {
+    // console.log(countChar());
+    // console.log(remainingChar());
+   totalEl.innerText = countChar();
+   remainingEl.innerText = remainingChar(); 
+})
 
